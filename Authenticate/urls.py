617518c7ccfc4c3c1dbf42102e0713f2fe94b40f
Yeswanthpg <yeswanthpg@gmail.com>
2024-01-12 15:28:18 +0530

    updated

--- Authenticate/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from Authenticate.views import *
-
-urlpatterns = [
-    path('',userlogin,name='login'),
-    path('Authenticate/register',register,name='register'),
-    path('Admin/admin-dashboard',admin_dashboard,name='admin-dashboard'),
-]
--- Farmer/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def Farmer(request):
-    return render(request, 'Farmer.html')
