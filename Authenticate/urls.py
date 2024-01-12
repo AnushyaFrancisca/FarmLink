8afa7b132aee034ba9c9b627e32ce73c0087948d
Yeswanthpg <yeswanthpg@gmail.com>
2024-01-12 19:14:28 +0530

    update

--- Authenticate/urls.py
+++ Authenticate/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from Authenticate.views import *
 
 urlpatterns = [
-    path('login',userlogin,name='login'),
+    path('',userlogin,name='login'),
     path('Authenticate/register',register,name='register'),
     path('Admin/admin-dashboard',admin_dashboard,name='admin-dashboard'),
     path('Admin/about',about,name='about'),
-    path('',base,name='base'),
+    path('Authenticate/base',base,name='base'),
 ]
