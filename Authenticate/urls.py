bae95c31a113ce21c3876f6f919084f0d4b0e371
AnushyaFranscisca <anushya.francisca@msds.christunniversity.in>
2024-01-13 10:33:52 +0530

    linked base with about us page

--- Authenticate/urls.py
+++ Authenticate/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from Authenticate.views import *
 
 urlpatterns = [
-    path('',userlogin,name='login'),
+    path('Login',userlogin,name='login'),
     path('Authenticate/register',register,name='register'),
     path('Admin/admin-dashboard',admin_dashboard,name='admin-dashboard'),
     path('Admin/about',about,name='about'),
-    path('Authenticate/base',base,name='base'),
+    path('',base,name='base'), #Authenticate/base
 ]
