8db689640869dbddd5f4f850c6644110c38eb2e4
AnushyaFranscisca <anushya.francisca@msds.christunniversity.in>
2024-02-05 00:09:07 +0530

    Created separate pages for policies and government

--- Government/urls.py
+++ Government/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('official-profile/',views.Government,name="official-profile"),
     path('government-profile',views.Government,name="government-profile"),
     path('post',views.post,name="post"),
-    path('/policies',views.policies,name="policies"),
+    path('policies/',views.policies,name="policies"),
     path('logout/',logout_view,name='logout')
 ]
